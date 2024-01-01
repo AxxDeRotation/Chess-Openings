0797239aa930d1b2bd9ecf4a9750c386c65b91fa
Axel <axel.montlahuc@stjoavignon.com>
2024-01-01 13:01:24 +0100

    Finished homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import '../styles/global.css';
+    import logo from "$lib/img/logo.png";
+</script>
+
+<header class="navbar">
+    <a href="/" class="navbar_logo">
+        <img src={logo} alt="Logo">
+    </a>
+
+    <nav class="nav-links">
+        <a href="/">Explore</a>
+        <a href="/">My Openings</a>
+        <a href="/">My Repertoires</a>
+        <button>Log In</button>
+    </nav>
+</header>
+
+<slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Chess Openings</title>
+</svelte:head>
+
+<script>
+    import '../styles/home.css';
+    import chessboard from "$lib/img/chessboard_red.png";
+</script>
+
+<div class="presentation">
+    <div class="hero-container">
+        <h1>Chess Openings</h1>
+        <p>Explore chess openings and learn how to play them.</p>
+        <div>
+            <button>Learn an Opening</button>
+            <button>Explore Repertoires</button>
+        </div>
+    </div>
+
+    <img src={chessboard} alt="Chess Board">
+</div>
