c5e76ce8b462626726643b7607de7726e1642a28
Axel <axel.montlahuc@stjoavignon.com>
2024-01-04 16:17:57 +0100

    Added the Lichess board at /chessground

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,23 @@
 <script>
     import '../styles/global.css';
     import logo from "$lib/img/logo.png";
+    import { onMount } from "svelte";
+    import "$lib/firebase.js"
+    import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+    /**
+	 * @type {import("@firebase/auth").User | null}
+	 */
+    let user = null;
+
+    onMount(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        user = currentUser;
+    });
+
+    return unsubscribe; // Unsubscribe from the listener when the component is destroyed
+    });
 </script>
 
 <header class="navbar">
@@ -12,9 +29,13 @@
         <a href="/openings">Explore</a>
         <a href="/">My Openings</a>
         <a href="/">My Repertoires</a>
+        {#if user}
+            <button>Logged In</button>
+        {:else}
         <a href="/login">
             <button>Log In</button>
         </a>
+        {/if}
     </nav>
 </header>
 
---
+++ src/routes/chessground/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {Chess} from 'svelte-chess';
+</script>
+
+<main>
+    <Chess />
+</main>
+
+<style>
+    main {
+        position: absolute;
+        top: 54%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 600px;
+        height: 600px;
+    }
+</style>
